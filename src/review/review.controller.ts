01c646d0dfe49570653dae29b611746a7f7443d9
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-06-27 15:53:40 +0300

    Add authorization strategy

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -8,11 +8,13 @@ import {
   HttpStatus,
   Param,
   Post,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { REVIEW_NOT_FOUND } from './review.constants';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('review')
 export class ReviewController {
@@ -32,6 +34,7 @@ export class ReviewController {
     }
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('byProduct/:productId')
   async getByProduct(@Param('productId') productId: string) {
     return this.reviewService.findByProductId(productId);
