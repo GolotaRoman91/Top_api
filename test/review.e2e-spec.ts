df0585e6128907444c9c33a7aca9d50392b35908
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-07-03 13:11:44 +0300

    Fix e2e tests

--- test/review.e2e-spec.ts
+++ test/review.e2e-spec.ts
@@ -5,8 +5,14 @@ import { AppModule } from '../src/app.module';
 import { CreateReviewDto } from 'src/review/dto/create-review.dto';
 import { Types, disconnect } from 'mongoose';
 import { REVIEW_NOT_FOUND } from '../src/review/review.constants';
+import { AuthDto } from '../src/auth/dto/auth.dto';
 
 const productId = new Types.ObjectId().toHexString();
+
+const loginDto: AuthDto = {
+  login: 'newUser@mail.com',
+  password: 'test1234',
+};
 
 const testDto: CreateReviewDto = {
   name: 'test',
@@ -26,6 +32,7 @@ const testDtoWithoutName: Omit<CreateReviewDto, 'name'> = {
 describe('AppController (e2e)', () => {
   let app: INestApplication;
   let createdId: string;
+  let token: string;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -34,6 +41,11 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    const { body } = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send(loginDto);
+    token = body.access_token;
   });
 
   it('/review/create (POST) - success', async () => {
@@ -53,31 +65,35 @@ describe('AppController (e2e)', () => {
       .expect(400);
   });
 
-  it('/review/:id (GET) - success', async () => {
+  it('/review/byProduct/:productId (GET) - success', async () => {
     const response = await request(app.getHttpServer())
       .get('/review/byProduct/' + productId)
-      .expect(200);
+      .set('Authorization', 'Bearer ' + token);
 
+    expect(response.status).toBe(200);
     expect(response.body.length).toBe(1);
   });
 
-  it('/review/:id (GET) - fail', async () => {
+  it('/review/byProduct/:productId (GET) - fail', async () => {
     const response = await request(app.getHttpServer())
       .get('/review/byProduct/' + new Types.ObjectId().toHexString())
-      .expect(200);
+      .set('Authorization', 'Bearer ' + token);
 
+    expect(response.status).toBe(200);
     expect(response.body.length).toBe(0);
   });
 
   it('/review/:id (DELETE) - success', () => {
     return request(app.getHttpServer())
       .delete('/review/' + createdId)
+      .set('Authorization', 'Bearer ' + token)
       .expect(200);
   });
 
   it('/review/:id (DELETE) - fail', () => {
     return request(app.getHttpServer())
       .delete('/review/' + new Types.ObjectId().toHexString())
+      .set('Authorization', 'Bearer ' + token)
       .expect(404, {
         statusCode: 404,
         message: REVIEW_NOT_FOUND,
